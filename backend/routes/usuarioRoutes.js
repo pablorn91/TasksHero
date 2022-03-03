1612a1037a8b1e67295fe330c97b0ca399a65337
Pablo Rodriguez Nunes <pmrn24@gmail.com>
2022-03-03 09:07:21 -0400

    ruta login, autenticado y validacion

--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 const router = express.Router();
-import { registrar } from '../controllers/usuarioController.js';
+import { registrar, autenticar } from '../controllers/usuarioController.js';
 
 //Autenticación, registro y confirmación de usuarios
 router.post('/', registrar); //Crea un nuevo usuario
+router.post('/login', autenticar); //autenticar usuario
  
 export default router;
