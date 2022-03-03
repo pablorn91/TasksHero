ac782e6be7457c2b51fdf6d89cf239c91acc7ded
Pablo Rodriguez Nunes <pmrn24@gmail.com>
2022-03-03 16:23:55 -0400

    Modelo, Controlador y Rutas para Proyectos

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import conectarDB from './config/db.js';
 import usuarioRoutes from './routes/usuarioRoutes.js';
+import proyectoRoutes from './routes/proyectoRoutes.js';
 // const express = require('express'); <<< sintaxis anterior
 
 const app = express();
@@ -12,6 +13,7 @@ dotenv.config();
 conectarDB();
 
 app.use('/api/usuarios', usuarioRoutes)
+app.use('/api/proyectos', proyectoRoutes)
 
 const PORT = process.env.PORT || 4000; //revia si existe variable de entorono sino asigna 4000
 
