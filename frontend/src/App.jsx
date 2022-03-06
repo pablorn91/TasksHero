83b7ba32631a113f8f59fa0e77315b92eae06b10
Pablo Rodriguez Nunes <pmrn24@gmail.com>
2022-03-06 16:56:24 -0400

    context y hook para Auth y rutaprotegida

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,21 +5,30 @@ import Registrar from './pages/Registrar'
 import OlvidePassword from './pages/OlvidePassword'
 import NuevoPassword from './pages/NuevoPassword'
 import ConfirmarCuenta from './pages/ConfirmarCuenta'
+import RutaProtegida from './layouts/RutaProtegida'
+import Proyectos from './pages/Proyectos'
+import { AuthProvider } from './context/AuthProvider'
 
 function App() {
   
   return (
    <BrowserRouter>
-      <Routes>
-          <Route path='/' element={<AuthLayout/>}>
-            <Route index element={<Login />} />
-            <Route path='registrar' element={<Registrar />} />
-            <Route path='olvide-password' element={<OlvidePassword />} />
-            <Route path='olvide-password/:token' element={<NuevoPassword />} />
-            <Route path='confirmar/:id' element={<ConfirmarCuenta />} />
+    <AuthProvider>
+        <Routes>
+            <Route path='/' element={<AuthLayout/>}>
+              <Route index element={<Login />} />
+              <Route path='registrar' element={<Registrar />} />
+              <Route path='olvide-password' element={<OlvidePassword />} />
+              <Route path='olvide-password/:token' element={<NuevoPassword />} />
+              <Route path='confirmar/:id' element={<ConfirmarCuenta />} />
+            </Route>
 
-          </Route>
-      </Routes>
+            <Route path='/proyectos' element={<RutaProtegida/>}>
+                <Route index element={<Proyectos/>}/>
+            </Route>
+
+        </Routes>
+    </AuthProvider>
    </BrowserRouter>
   )
 }
